64d4440272589fabc53d44e3d9bf15d9febc1a7b
Himanshu wasnik <141391153+himwasnik@users.noreply.github.com>
2024-11-29 18:00:49 +0530

    variables.tf

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "client_id" {
+  description = "Client ID for the service principal"
+  type        = string
+}
+
+variable "client_secret" {
+  description = "Client Secret for the service principal"
+  type        = string
+}
+
+variable "tenant_id" {
+  description = "Tenant ID"
+  type        = string
+}
+
+variable "subscription_id" {
+  description = "Subscription ID"
+  type        = string
+}
